67c90b69aab5679a2237eac305f761aebff9d97c
Cynthia Wilhelm & Tom Mertz <student@epicodus.com>
2015-04-13 15:58:42 -0700

    Create functionality to move tasks back and forth between inprogress and completed lists

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,37 @@
 $(document).ready(function() {
   $("#addTask").submit(function(event) {
+
     var taskInput = $("input#task").val();
 
     var task = {item: taskInput};
 
-    $(".inprogress").append("<li><span class='glyphicon glyphicon-ok clickable'></span>" + task.item + "</li>").addClass("in-progress");
+    $(".inprogress").append("<li><span class='glyphicon glyphicon-ok clickable addTask'></span>" + task.item + "</li>");
 
-    $(".clickable").last().click(function() {
+    $(".addTask").last().click(function() {
       $(this).parent().remove();
-      var testTaskItem = task.item;
-      $(".complete").prepend("<li>" + task.item + "</li>").addClass("complete");
+      $(".complete").prepend("<li><span class='glyphicon glyphicon-arrow-left clickable renewTask'></span><span class='glyphicon glyphicon-remove deleteTask clickable'></span>" + task.item + "</li>");
+      makeCompleteListeners();
     });
 
     $("input#task").val("").focus();
-
-
     event.preventDefault();
   });
-
-
-
-
 });
+
+var makeCompleteListeners = function() {
+  $('.renewTask').unbind('click');
+  $('.renewTask').click(function() {
+    $(".inprogress").append("<li><span class='glyphicon glyphicon-ok clickable addTask'></span>" + $(this).parent().text() + "</li>");
+    $(this).parent().remove();
+    makeProgressListeners();
+  });
+};
+
+var makeProgressListeners = function() {
+  $('.addTask').unbind('click');
+  $('.addTask').click(function() {
+    $(this).parent().remove();
+    $(".complete").prepend("<li><span class='glyphicon glyphicon-arrow-left clickable renewTask'></span><span class='glyphicon glyphicon-remove deleteTask clickable'></span>" + $(this).parent().text() + "</li>");
+    makeCompleteListeners();
+  });
+};
